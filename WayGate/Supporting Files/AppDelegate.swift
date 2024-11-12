79abd32b4e488ce4ef06d9292b044ee3974cd4ae
Nabeel Nazir <nabeel.nazir@jeeny.me>
2024-11-12 23:38:45 +0500

    no message

--- WayGate/Supporting Files/AppDelegate.swift
+++ WayGate/Supporting Files/AppDelegate.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        AVCaptureDevice.requestAccess(for: .video) { granted in
+            
+        }
         return true
     }
 
